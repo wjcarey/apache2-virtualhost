cceaec91533344ef1b12d4ebe3f98f8b9ad88506
WJ Carey <contact@wjcarey.io>
2020-10-16 14:38:08 -0600

    changed variable names

--- apache2-virtualhost.sh
+++ apache2-virtualhost.sh
@@ -3,27 +3,29 @@
 if [ -z "$1" ]
     then
         echo "enter the webroot of your website ..."
-        read WEB_ROOT
+        read USER_PATH
         echo "enter the domain name of your website ..."
-        read INSTALL_DOMAIN
+        read DOMAIN_NAME
     else
-    WEB_ROOT=${1}
-    INSTALL_DOMAIN=${2}
+    USER_PATH=${1}
+    DOMAIN_NAME=${2}
 fi
 
+INSTALL_PATH=$(realpath -s --canonicalize-missing $USER_PATH)
+
 #apache2 add virtual host
-touch /etc/apache2/sites-available/${INSTALL_DOMAIN}
+touch /etc/apache2/sites-available/${DOMAIN_NAME}
 
 printf "
 <VirtualHost *:80>
     ServerAdmin admin@example.com
-    ServerName ${INSTALL_DOMAIN}
-    ServerAlias www.${INSTALL_DOMAIN}
-    DocumentRoot ${WEB_ROOT}
+    ServerName ${DOMAIN_NAME}
+    ServerAlias www.${DOMAIN_NAME}
+    DocumentRoot ${INSTALL_PATH}
     ErrorLog /var/log/apache2/error.log
     CustomLog /var/log/apache2/access.log combined
 </VirtualHost>
-" >> /etc/apache2/sites-available/${INSTALL_DOMAIN}.conf
+" >> /etc/apache2/sites-available/${DOMAIN_NAME}.conf
 
 a2ensite ${INSTALL_DOMAIN}.conf
 systemctl restart apache2
