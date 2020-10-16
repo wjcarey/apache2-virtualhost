fe7cf3f0c0ccb2729facacebb3f2aaed7990b8aa
WJ Carey <contact@wjcarey.io>
2020-10-16 13:51:03 -0600

    init file upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apache2-virtualhost
 Add a virtual host file to apache2
+~~~
+sudo curl -o apache2-virtualhost.sh https://raw.githubusercontent.com/wjcarey/apache2-virtualhost/master/apache2-virtualhost.sh && sudo chmod 777 apache2-virtualhost.sh && sudo ./apache2-virtualhost.sh
+~~~
---
+++ apache2-virtualhost.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+    then
+        echo "enter the webroot of your website ..."
+        read WEB_ROOT
+        echo "enter the domain name of your website ..."
+        read INSTALL_DOMAIN
+    else
+    WEB_ROOT=${1}
+    INSTALL_DOMAIN=${2}
+fi
+
+#apache2 add virtual host
+touch /etc/apache2/sites-available/${INSTALL_DOMAIN}
+
+printf "
+<VirtualHost *:80>
+    ServerAdmin admin@example.com
+    ServerName ${INSTALL_DOMAIN}
+    ServerAlias www.${INSTALL_DOMAIN}
+    DocumentRoot ${WEB_ROOT}
+    ErrorLog /var/log/apache2/error.log
+    CustomLog /var/log/apache2/access.log combined
+</VirtualHost>
+" >> /etc/apache2/sites-available/${INSTALL_DOMAIN}.conf
+
+systemctl restart apache2
+
+#SELF DELETE AND EXIT
+rm -- "$0"
+exit
